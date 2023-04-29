cd4a58c5717738f4f71c434a5da4791abf5e590e
Dipti77 <diptimhandlekar123@gmail.com>
2023-04-29 21:04:03 +0530

    Added: Checking equality of two lines using equals method

--- LineComparison.java
+++ LineComparison.java
@@ -1,15 +1,26 @@
-
 package com.bridgelabz;
 
 import java.util.Scanner;
 
 public class LineComparison {
 
-	static void calculateLength(int x1, int y1, int x2, int y2)
+	static void calculateLength(int x1, int y1, int x2, int y2, int a1, int b1, int a2, int b2)
 	{
 		
-		double length = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
-		System.out.println("Length of a line: " + length);
+		Double line1Length = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+		System.out.println("Length of line 1: " + line1Length);
+		
+		Double line2Length = Math.sqrt(Math.pow(a2-a1, 2)+Math.pow(b2-b1, 2));
+		System.out.println("Length of line 2: " + line2Length);
+		
+		
+		if (line1Length.equals(line2Length) == true) {
+			System.out.println("Both lines are equal");
+		}
+		else {
+			System.out.println("Both lines are not equal");
+		}
+		
 	}
 
 	public static void main(String[] args) {
@@ -23,10 +34,17 @@ public class LineComparison {
 		int x2 = sc.nextInt();
 		System.out.println("Enter y2 co-ordinate: ");
 		int y2 = sc.nextInt();
+		System.out.println("Enter a1 co-ordinate: ");
+		int a1 = sc.nextInt();
+		System.out.println("Enter b1 co-ordinate: ");
+		int b1 = sc.nextInt();
+		System.out.println("Enter a2 co-ordinate: ");
+		int a2 = sc.nextInt();
+		System.out.println("Enter b2 co-ordinate: ");
+		int b2 = sc.nextInt();
 		
-		calculateLength(x1, y1, x2, y2);
+		calculateLength(x1, y1, x2, y2, a1, b1, a2, b2);
 		
 	}
-	
 }
 
